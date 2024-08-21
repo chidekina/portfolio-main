1de9f08f62d4f874071a2c7de092d87f480295a4
CESAR HIDEKI NAGANO <cesar0610@digitalcollegesul.local>
2024-08-21 11:41:22 -0300

    responsiveness adjustment

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function mostrarConteudo_descricao(){
     let conteudo = document.getElementById("apresentacao__conteudo__descricao");
     let projetos = document.getElementById("apresentacao__conteudo__projetos");
+    let w = parseInt(window.innerWidth);
 
+    if(w <= 600){
+        
+    }
+    else if(w > 600 && w <= 768){}
+    else if(w > 768 && w <= 1024){}
+    
     conteudo.style.opacity = "1";
     conteudo.style.height = "auto";
     projetos.style.opacity = "0";
